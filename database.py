8d615c1471ada6512c97cfe29810bcabc22c75e2
sofyasochor <133574823+sofyasochor@users.noreply.github.com>
2023-06-10 00:48:48 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,45 +1,42 @@
 import sqlite3
 
 
-def get_conn():
-    # Подключаемся к базе данных
-    conn = sqlite3.connect('example.db')
-    return conn
+class Database:
+
+    def get_conn(self):
+        # Подключаемся к базе данных
+        conn = sqlite3.connect('example.db')
+        return conn
 
 
-def create_table():
-    conn = get_conn()
-    # Создаем таблицу
-    conn.execute('''CREATE TABLE IF NOT EXISTS users
-    (
-    id INTEGER
-    );
-    ''')
-    conn.commit()
-    conn.close()
+    def create_table(self):
+        conn = self.get_conn()
+        # Создаем таблицу
+        conn.execute('''CREATE TABLE IF NOT EXISTS users
+        (
+        id INTEGER
+        );
+        ''')
+        conn.commit()
+        conn.close()
 
 
-def insert_table(id_):
-    conn = get_conn()
-    # Вставляем новые данные в таблицу
-    conn.execute(f"INSERT INTO users VALUES ({id_})")
-    # Фиксируем транзакцию
-    conn.commit()
-    conn.close()
+    def insert_table(self, id_):
+        conn = self.get_conn()
+        # Вставляем новые данные в таблицу
+        conn.execute(f"INSERT INTO users VALUES ({id_})")
+        # Фиксируем транзакцию
+        conn.commit()
+        conn.close()
 
 
-def user_in_table(id_):
-    conn = get_conn()
-    user = conn.execute(f"SELECT * FROM users WHERE id={id_}")
-    user = user.fetchone()
-    conn.close()
+    def user_in_table(self, id_):
+        conn = self.get_conn()
+        user = conn.execute(f"SELECT * FROM users WHERE id={id_}")
+        user = user.fetchone()
+        conn.close()
 
-    if user is not None:
-        return True
+        if user is not None:
+            return True
 
-    return False
-
-
-if __name__ == "__main__":
-    create_table()
-
+        return False
